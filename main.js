49429ac1f79dadeb241d575911eda1988fb81f62
Vic Pues <141432283+vicpues@users.noreply.github.com>
2024-12-27 17:45:50 +0100

    test: Add tests from the assignment

--- main.js
+++ main.js
@@ -2,50 +2,35 @@ const HashMap = require("./hashmap");
 
 const table = new HashMap();
 
-// ["Elephant", "cheetah", "lion", "gnu", "gazelle", "Zebra", "Giraffe"].forEach(
-//     (animal) => console.log(`${animal.padStart(10)}: ${table.hash(animal)}`),
-// );
-
-const animals = {
-    Elephant: "leaves",
-    cheetah: "meat",
-    lion: "meat",
-    gnu: "grass",
-    gazelle: "grass",
-    Zebra: "grass",
-    Giraffe: "leaves",
+const colors = {
+    apple: "red",
+    banana: "yellow",
+    carrot: "orange",
+    dog: "brown",
+    elephant: "gray",
+    frog: "green",
+    grape: "purple",
+    hat: "black",
+    "ice cream": "white",
+    jacket: "blue",
+    kite: "pink",
+    lion: "golden",
 };
 
-for (let diet in animals) table.set(diet, animals[diet]);
-console.log(table.get("Elephant")); // leaves
-console.log(table.get("Zebra")); // grass
-console.log(table.get("gnu")); // grass
-console.log(table.get("dragon!")); // null
+for (let thing in colors) {
+    table.set(thing, colors[thing])
+}
+
+table.set("apple", "green");
+console.log(table.get("apple"), table.length); // green 12
 console.log();
 
-table.set("Elephant", "leaves AND grass");
-console.log(table.get("Elephant")); // leaves AND grass
+table.set("dog", "black");
+console.log(table.get("dog"), table.length); // black 12
 console.log();
 
-console.log(table.has("Zebra")); // true
-console.log(table.has("dragon")); // false
+console.log(table.load.toFixed(3)); // 0.706
+table.set("moon", "silver");
+console.log(table.get("moon"), table.length); // silver 13
+console.log(table.load.toFixed(3)); // 0.353
 console.log();
-
-console.log(table.remove("gazelle")); // true
-console.log(table.get("gazelle")); // null
-console.log(table.remove("gazelle")); // false
-console.log();
-
-console.log(table.length); // 6
-console.log();
-
-console.log(table.keys()); // [ 'cheetah', 'gnu', 'Elephant', 'Giraffe', 'Zebra', 'lion' ]
-console.log();
-
-console.log(table.values()); // [ 'meat', 'grass', 'leaves AND grass', 'leaves', 'grass', 'meat' ]
-console.log();
-
-console.log(table.entries()); // [ [ 'cheetah', 'meat' ], [ 'gnu', 'grass' ], [ 'Elephant', 'leaves AND grass' ], [ 'Giraffe', 'leaves' ], [ 'Zebra', 'grass' ], [ 'lion', 'meat' ] ]
-console.log();
-
-console.log(table.clear().length); // 0
