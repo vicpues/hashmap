6ca95e9d77c9bcc38f06768a7cd3a2d55f026743
Vic Pues <141432283+vicpues@users.noreply.github.com>
2024-12-27 14:50:54 +0100

    test: Add tests for get() and set()

--- main.js
+++ main.js
@@ -2,6 +2,25 @@ const HashMap = require("./hashmap");
 
 const table = new HashMap();
 
-["Elephant", "cheetah", "lion", "gnu", "gazelle", "Zebra", "Giraffe"].forEach(
-    (animal) => console.log(`${animal.padStart(10)}: ${table.hash(animal)}`),
-);
+// ["Elephant", "cheetah", "lion", "gnu", "gazelle", "Zebra", "Giraffe"].forEach(
+//     (animal) => console.log(`${animal.padStart(10)}: ${table.hash(animal)}`),
+// );
+
+const animals = {
+    Elephant: "leaves",
+    cheetah: "meat",
+    lion: "meat",
+    gnu: "grass",
+    gazelle: "grass",
+    Zebra: "grass",
+    Giraffe: "leaves",
+};
+
+for (let diet in animals) table.set(diet, animals[diet]);
+console.log(table.get("Elephant")); // leaves
+console.log(table.get("Zebra")); // grass
+console.log(table.get("gnu")); // grass
+console.log(table.get("dragon!")); // null
+
+table.set("Elephant", "leaves AND grass");
+console.log(table.get("Elephant")); // leaves AND grass
