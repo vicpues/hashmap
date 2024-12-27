084c6cafdd49b39127b1f263b165e09817c9d73c
Vic Pues <141432283+vicpues@users.noreply.github.com>
2024-12-27 18:00:31 +0100

    test: Write new tests for all methods

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const colors = {
 };
 
 for (let thing in colors) {
-    table.set(thing, colors[thing])
+    table.set(thing, colors[thing]);
 }
 
 table.set("apple", "green");
@@ -34,3 +34,48 @@ table.set("moon", "silver");
 console.log(table.get("moon"), table.length); // silver 13
 console.log(table.load.toFixed(3)); // 0.353
 console.log();
+
+table.set("hat", "red & green");
+console.log(table.get("hat"), table.length); // red & green 13
+console.log();
+
+console.log(table.get("dinosaur")); // null
+console.log();
+
+console.log(table.has("apple")); // true
+console.log(table.has("dinosaur")); // false
+console.log();
+
+table.remove("kite");
+console.log(table.has("kite")); // false
+console.log();
+
+console.log(table.length); // 12
+console.log();
+
+console.log(table.keys()); // [ 'banana', 'apple', 'moon', 'hat', 'frog', 'ice cream', 'lion', 'grape', 'dog', 'jacket', 'elephant', 'carrot' ]
+console.log();
+
+console.log(table.values()); // [ 'yellow', 'green', 'silver', 'red & green', 'green', 'white', 'golden', 'purple', 'black', 'blue', 'gray', 'orange' ]
+console.log();
+
+console.log(table.entries());
+/*
+    [
+        [ 'banana', 'yellow' ],
+        [ 'apple', 'green' ],
+        [ 'moon', 'silver' ],
+        [ 'hat', 'red & green' ],
+        [ 'frog', 'green' ],
+        [ 'ice cream', 'white' ],
+        [ 'lion', 'golden' ],
+        [ 'grape', 'purple' ],
+        [ 'dog', 'black' ],
+        [ 'jacket', 'blue' ],
+        [ 'elephant', 'gray' ],
+        [ 'carrot', 'orange' ]
+    ]
+*/
+console.log();
+
+console.log(table.clear().entries(), table.length); // [] 0
