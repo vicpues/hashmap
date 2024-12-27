fb8cbafe21730182aba06c500e1a306a7709c617
Vic Pues <141432283+vicpues@users.noreply.github.com>
2024-12-27 14:50:40 +0100

    feat: Add get() and set() methods

--- hashmap.js
+++ hashmap.js
@@ -1,6 +1,9 @@
+const LinkedList = require("./linked-list");
+
 class HashMap {
     #capacity = 17;
     #loadFactor = 0.8;
+    #buckets = Array.from({ length: this.#capacity }, () => new Bucket());
 
     /**
      * Takes in a string and returns a hashed number
@@ -11,9 +14,56 @@ class HashMap {
         const primeNumber = 31;
         for (let i = 0; i < string.length; i++) {
             hashCode = primeNumber * hashCode + string.charCodeAt(i);
-            hashCode = hashCode % this.#capacity 
+            hashCode = hashCode % this.#capacity;
         }
         return hashCode;
+    }
+
+    /**
+     * Sets the key to the specified value
+     * @param {string} key The key for the value
+     * @param {any} value The value to be stored
+     */
+    set(key, value) {
+        // TODO: grow buckets if reaching loadfactor
+        const bucket = this.#getBucket(key);
+        const index = bucket.find(key);
+        const pair = { key, value };
+        if (index === null) bucket.append(pair);
+        else bucket.replaceAt(pair, index);
+        return this;
+    }
+
+    /**
+     * Gets the value at the specified key
+     * @param {string} key The key we're retrieving
+     */
+    get(key) {
+        const bucket = this.#getBucket(key);
+        const index = bucket.find(key);
+        if (index === null) return null;
+        return bucket.at(index).value;
+    }
+
+    // PRIVATE METHODS
+
+    /**
+     * Returns the bucket that contains a key
+     * @param {string} key The key whose hash is the index of the bucket
+     */
+    #getBucket(key) {
+        const hashCode = this.hash(key);
+        return this.#buckets[hashCode];
+    }
+}
+
+class Bucket extends LinkedList {
+    /** Finds an object where the property object.key === key; if none is found, returns null
+     * @param {string} key
+     */
+    find(key) {
+        const callback = (item) => item.key;
+        return super.find({ key }, callback);
     }
 }
 
